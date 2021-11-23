f99441d7d130dcbd7e108f7aba11a717eae293a5
ikjnv <abdurauf.isokjonov@gmail.com>
2021-11-23 17:58:57 +0900

    displaying chart
    * displaying coin market data in chart

--- src/components/CoinBlock/Chart.tsx
+++ src/components/CoinBlock/Chart.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import * as d3 from 'd3';
+import {scaleUtc} from 'd3';
 
-interface Props {
+interface CoinProps {
 	id:		string;
 	name:	string;
 	symbol:	string;
@@ -20,7 +22,7 @@ function convert(arg: ChartProps[]) {
 	const temp: any = [];
 	arg.map((o: any) => {
 		const d = {
-			date: new Date(o[0]).toISOString(),
+			date: new Date(o[0]),
 			price: o[1]
 		}
 		temp.push(d);
@@ -28,8 +30,10 @@ function convert(arg: ChartProps[]) {
 	return temp;
 };
 
-const ChartBlock = ({ id, name, symbol }: Props) => {
+const ChartBlock = ({ id, name, symbol }: CoinProps) => {
 	const [chartData, setChartData] = React.useState<ConvertedChartProps[]>();
+	const ref = React.useRef<HTMLDivElement>(null);
+	const height = 400;
 
 	React.useEffect(() => {
 		try {
@@ -42,19 +46,67 @@ const ChartBlock = ({ id, name, symbol }: Props) => {
 		}
 	}, [id]);
 
-	console.log('data', chartData);
+	React.useEffect(() => {
+		if (chartData) {
+			const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+			const width = height / 1.6;
+			const currentElement = ref.current;
+			const svg = d3.select(currentElement);
+
+			const documentElement = d3.select(currentElement)
+				.call(g => g.select('svg').remove())
+				.append('svg')
+				.attr('viewBox', `0, 0, ${width}, ${height}`);
+
+			const d3Type: Function = d3.line()
+				.x((value: any) => x(value.date))
+				.y((value: any) => y(value.price));
+
+			const xDomain = [
+				new Date('2021-11-16'),
+				new Date('2021-11-23')
+			];
+
+			const x = d3.scaleUtc()
+				.domain(xDomain)
+				.range([margin.left, width - margin.right]);
+
+			const yMax = d3.max(chartData, d => d.price);
+			const y = d3.scaleLinear()
+				.domain([0, yMax]).nice()
+				.range([height - margin.bottom, margin.top]);
+
+			const xAxis = (g: any) => g
+				.attr("transform", `translate(0, ${height - margin.bottom})`)
+				.call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+
+			documentElement.append<SVGGElement>('g')
+				.call(xAxis);
+
+			const yAxis = (g: any) => g
+				.attr("transform", `translate(${margin.left}, 0)`)
+				.call(d3.axisLeft(y));
+
+			documentElement.append<SVGGElement>('g')
+				.call(yAxis)
+				.call(g => g.select(".domain").remove());
+
+			documentElement.append('path')
+				.datum(chartData)
+				.attr('fill', 'none')
+				.attr('stroke', 'steelblue')
+				.attr('stroke-width', 1.5)
+				.attr('stroke-linejoin', 'round')
+				.attr('stroke-linecap', 'round')
+				.attr('d', chartData => d3Type(chartData));
+		}
+	}, [chartData]);
 
 	return (
 		<>
-			{ chartData && chartData.map((ch, i) => (
-				<div key={i}>
-					<p>
-						<span>{ch.price}</span> - 
-						<span>{ch.date}</span>
-					</p>
-				</div>
-			))
-			}
+			<h4>Line chart</h4>
+			<div ref={ref} style={{ width: '100%', height: height }}>
+			</div>
 		</>
 	);
 }
